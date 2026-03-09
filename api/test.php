47acd5ee5be5ca61e8c0c6d27e78c24d2e513c8a
David Martin <104839459+damartin18@users.noreply.github.com>
2026-03-09 16:18:32 -0500

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,3 +1,9 @@
+<?php
+echo "DB_HOST=" . var_export(getenv("DB_HOST"), true) . "<br>";
+echo "DB_PORT=" . var_export(getenv("DB_PORT"), true) . "<br>";
+echo "DB_USER=" . var_export(getenv("DB_USER"), true) . "<br>";
+echo "DB_NAME=" . var_export(getenv("DB_NAME"), true) . "<br>";
+?>
 
 
 
